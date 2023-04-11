e507495e13b10edca298cf3d5922733c0972b1d3
28vikas <63vikas@gmail.com>
2023-04-11 18:51:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,139 @@
+
+
+
+
+
+
+// Question number 1: Write an if statement that checks if a number is greater than 5.
+
+// let x = 8;
+
+// if(x > 5){
+//     console.log("the number " +x+ " is greater than 5");
+// }else{
+//     console.log("the number " +x+ " is not greater than 5");
+// }
+
+
+
+// Question number 2: 	Write an if statement that checks if a string is empty.
+
+// let x = ""
+
+// if(x.length == 0){
+//    console.log("the string is empty");
+// }else{
+//     console.log("the string is not empty");
+// }
+
+
+// Question number 3:Write an if-else statement that checks if a number is even or odd.
+
+// let num = 17;
+
+// if(num%2 == 0){
+//     console.log("the number " +num+ " is even number");
+// }else{
+//     console.log("the number " +num+ " is odd number");
+// }
+
+
+// Question number 4: 4	Write a for loop that prints the numbers 1 to 10.
+
+//  for (let i = 0; i<=10; i++){
+//     console.log(i); 
+//  }
+
+// Question number 5: 5	Write a for loop that prints the even numbers between 1 and 10.
+
+//  for (let i = 1; i<=5; i++){
+//     console.log(i*2); 
+//  }
+
+// Question number 5(second method)
+
+//  for (let i = 1; i<=10; i++){
+//    if(i%2 == 0){
+//     console.log(i)
+//     }
+//  }
+
+// Question number 6:Write a for loop that prints the odd numbers between 1 and 10.
+
+//  for (let i = 1; i<=10; i++){
+//      if(i%2 == 0){
+//         continue
+//      }
+//      console.log(i);
+//  }
+
+// Question number 6 (second method)
+
+//  for (let i = 1; i<=10; i++){
+//      if(i%2 !== 0){
+//      console.log(i);
+//      }
+//  }
+
+// Question number 7:7	Write a function that takes in two numbers and returns their sum.
+
+// function sum(a,b){
+//     return total = a+b;
+// }
+// console.log(sum(20,40));
+
+
+// Question number 8: Write a function that takes in a string and returns the length of the string.
+
+// let nam = "vikas kumar"
+
+// function strlength(str){
+//     return nam.length;
+// }
+// console.log(strlength(nam))
+
+
+// Question number 9: Write a function that takes in an array and returns the length of the array.
+ 
+// let arr = ["vikas", "mohit", "rahul", "kachra"];
+
+// function arrlength(arr1){
+//           return arr.length;
+// }
+
+// console.log(arrlength(arr));
+
+
+// Question number 10: Write a function that takes in an object and returns the number of properties in the object.
+
+// let a = {
+//     fname : "vikas",
+//     lname : "kumar",
+//     age : 25,
+//     movies : ["3idot", "pk", "dhoom"]
+// }
+
+// function objectkey(objct){
+//         let count = 0;
+//         for(let elem in objct){
+//             count++;
+//         }
+//         return count;
+// }
+
+// console.log(objectkey(a));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ theory.js
@@ -0,0 +1,70 @@
+// Q1: what is an if statement in javascript
+// Ans: if statement is a conditinal statemnet. i.e true and false. if condition is true, the code inside the curly braces will be executed.if condition is false it skip the curly braces code.
+
+
+
+// Q2: what is the syntax for an if statement?
+// //  if(condition){
+//     true : code will be executed
+// // }
+
+
+// Q3: what is the syntax for an if-else statement in javascript?
+//   if(condition){
+//      true : code will be executed
+//   }else{
+//      false : code will be executed      
+//   }
+
+
+// Q4: if else statement to check positive and negative number
+// decalare variable num and asign any Number
+// after that use if-else statement
+// in if statement condtion num > 0 if it statisfied then code will execute for positive number otherwise else code
+// execute for negative number
+
+
+
+// Q5:if else statement to check even and odd number
+// decalare variable num and asign any Number
+// after that use if-else statement
+// in if statement condtion str.length == 0 if it statisfied then code will execute for empty string otherwise else code will be execute
+
+
+
+// Q6: How do you use an if-else statement to check if a string is empty or not?
+// decalare variable str and asign empty string ""
+// after that use if-else statement
+// in if statement condtion num%2 == 0 if it statisfied then code will execute for even number otherwise else code execute
+
+
+
+//Q7 How do you use an if-else statement to check if a variable is undefined or not?
+// decalare variable x without assigning
+// after that use if-else statement
+// in if statement condtion typeof x === "undefined" if it statisfied then code will execute for undefined otherwise else code execute
+
+
+
+//Q8 How do you use an if-else statement to check if a variable is null or not?
+// decalare variable x and assign null
+// after that use if-else statement
+// in if statement condtion  x === null if it statisfied then code will execute for null otherwise else code execute
+
+
+
+// Q9 How do you use an if-else statement to check if a variable is NaN or not?
+// decalare variable x and assign "hello"
+// after that use if-else statement
+// in if statement condtion (isNaN(x)) if it statisfied then code will execute for NaN otherwise else code execute
+
+
+
+//Q10 How do you use an if-else statement to check if two variables are equal or not?
+// decalare two variable x and y and assign different value
+// after that use if-else statement
+// in if statement condtion if x === y if it true then two variable are equal if code execute otherwise else code execute
+
+
+
+
